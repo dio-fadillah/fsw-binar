74790c9f1bafdd815ddadf6e88b2bbc471eeeab9
dio fadillah <dio.fadillah@gmail.com>
2021-10-04 12:24:54 +0700

    initializing

---
+++ site/assets/js/game-suit.js
@@ -0,0 +1,24 @@
+//generate random number
+let player = []
+
+function choose(element){
+    alert(element.getAttribute("value"));
+    let player = (element.getAttribute("value"));
+    console.log("yang dipilih adalah refresh " + element.getAttribute("value"));
+    console.log(player)
+} 
+
+
+//ambil pilihan computer
+let com = Math.random()
+console.log(com)
+//ambil pilihan player
+
+//logic game suitnynya
+
+
+
+
+
+
+
