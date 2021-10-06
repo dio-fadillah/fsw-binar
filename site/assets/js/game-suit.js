96d73518bf08697362cf88ddf9e1f92c26dbed43
dio fadillah <dio.fadillah@gmail.com>
2021-10-06 15:20:02 +0700

    upgrade with flex layouting

--- site/assets/js/game-suit.js
+++ site/assets/js/game-suit.js
@@ -1,20 +1,47 @@
-//generate random number
-let player = []
+let player = ""
+let com = ""
 
 function choose(element){
-    alert(element.getAttribute("value"));
-    let player = (element.getAttribute("value"));
-    console.log("yang dipilih adalah refresh " + element.getAttribute("value"));
-    console.log(player)
+    player = (element.getAttribute("value"));
+    com = Math.floor(Math.random() * 101);
+
+    //ambil pilihan computer
+    if (com <= 33 && com >0) {
+        console.log("Pilihan dari COM adalah BATU " + "value " + com);
+        com_choose = "BATU"
+    } else if (com >33 && com<=66){
+        console.log("Pilihan dari COM adalah GUNTING " + "value " + com);
+        com_choose = "GUNTING"
+    } else {
+        console.log("Pilihan dari COM adalah KERTAS " + "value " + com);
+        com_choose = "KERTAS"
+    }
+
+    //logic game suitnynya
+    if (player == com_choose){
+        alert("hasil seri" + "Player = " + player + "Computer = " + com_choose);
+        com_choose = ""
+        player = ""
+    } else if (
+        (player == "BATU" && com_choose == "GUNTING") || (player == "GUNTING" && com_choose == "KERTAS") || (player == "KERTAS" && com_choose == "BATU")
+        ){
+        alert("Player Menang " + "Player = " + player + "Computer = " + com_choose);
+        com_choose = ""
+        player = ""
+    } else {
+        alert("Player Kalah " + "Player = " + player + "Computer = " + com_choose);
+        com_choose = ""
+        player = ""
+    }
+
 } 
 
 
-//ambil pilihan computer
-let com = Math.random()
-console.log(com)
-//ambil pilihan player
 
-//logic game suitnynya
+
+
+
+
 
 
 
